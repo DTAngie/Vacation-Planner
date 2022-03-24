74f7efbd8a965fe6633be46258b9b98b59d09b7d
Angeline Teets <23041089+DTAngie@users.noreply.github.com>
2022-03-24 13:53:13 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+// add in when the app is ready to be deployed
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// app.use(require('./config/auth')); 
+
+//Routes
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app listening on port ${port}`);
+});
+
