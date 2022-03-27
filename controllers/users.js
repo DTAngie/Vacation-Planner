956152015d9bb4cc29d48299efed1319ee5e6629
Angeline Teets <23041089+DTAngie@users.noreply.github.com>
2022-03-27 12:41:44 -0700

    adds password hashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,10 @@
 const User = require('../models/user');
 const { v4: uuidv4 } = require('uuid');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { UNSAFE_NavigationContext } = require('react-router-dom');
+
+const SALT_ROUNDS = 6;
 
 module.exports = {
   signup
@@ -8,8 +12,11 @@ module.exports = {
 
 async function signup(req, res){
   console.log('made it back here');
+  const user = User.build(req.body);
   try {
-    const user = await User.create(req.body);
+    const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hashedPassword;
+    user.save()
     const token = createJWT(user);
     res.json({token});
   } catch(err) {
